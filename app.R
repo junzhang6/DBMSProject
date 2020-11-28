4ed2a733a24c964e9bedb9b0f29017b5fa308506
junzhang6 <zjj870422004@gmail.com>
2020-11-28 16:11:14 +0800

    Update

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(DBI)
 library(DT)
 library(tidyverse)
 
+
 dbcon <- dbConnect(SQLite(), dbname="5003Project.db")
 
 # Combine Tables
@@ -11,19 +12,15 @@ ProfClassCourse <- "SELECT C.classid, C.cid, P.pname, CR.ctitle, C.semester, C.y
           C.dsubject, CR.clevel FROM Professor AS P INNER JOIN Class AS C ON P.pid = C.pid INNER JOIN Course AS CR ON C.cid = CR.cid"
 tempTable <- dbGetQuery(dbcon, ProfClassCourse)
 
-# Create Table "Register"
-dbSendQuery(dbcon, "CREATE TABLE Register( sid char(20), classid char(20), PRIMARY KEY (sid, classid) FOREIGN KEY (sid) REFERENCES Student, 
-FOREIGN KEY (classid) REFERENCES Class)")
-
 
 ui <- navbarPage(title="DBMS Project", id="navbar", 
-                 
+
                  tabPanel("About", 
                           wellPanel(
                                 p(strong(h4("The idea of this application is to simulate the actual student registration system using DBMS, and 
                                 the data in our database are self-simulated.", 
                                             align="center"))),
-                                h3("To close this application, you MUST click the QUIT tab panel. (Otherwise the system might CRASH!)"), align="center",
+                                h3("To close this application, you MUST click the QUIT tab panel. (Otherwise cause trouble!!)"), align="center",
                                 p("You can find the codes for this application using the following link."), 
                                 HTML('<p> <a href="https://github.com/junzhang6/DBMSProject" 
                                       target="_blank">Link</a><br/> </p>')
@@ -103,27 +100,29 @@ ui <- navbarPage(title="DBMS Project", id="navbar",
                                          sidebarLayout(
                                                sidebarPanel(
                                                      numericInput(inputId="ClassID", label="Enter classid (You can select more than one course 
-                                                                  by entering the different IDs)", value=FALSE), 
+                                                                  by entering the different IDs). But you CANNOT enter the same classid twice! 
+                                                                  Also note that there might be a course that has been registered for you in
+                                                                  the system and you cannot change it.", 
+                                                                  value=FALSE), 
                                                      actionButton("ClassIDConfirm", label="Submit")
                                                ), # !!!!!! MOST IMPORTANT CONCEPT HERE !!!!!!
                                                mainPanel(
                                                      conditionalPanel(condition="input.ClassIDConfirm", 
                                                                       textOutput(outputId="EnrollMessage"),
-                                                                      tableOutput(outputId="EnrollOut"))
+                                                                      tableOutput(outputId="EnrollOut"),
+                                                                      textOutput(outputId="ErrorMessage"))
                                                )
                                          )
                                 ),
-                                # tabPanel("Drop",
-                                #          numericInput(inputId="CidDrop", label="Type in the course number that you want to drop"),
-                                #          textOutput(outputId="DropMessage")
-                                # ),
+                                
                                 tabPanel("Your Schedule",
                                          textOutput(outputId="StudentSchedule"),
                                          tableOutput(outputId="StudentScheduleTable")
                                 ),
                                 
                                 tabPanel("Drop",
-                                         numericInput(inputId="ClassDrop", label="If you want to drop any course, type in the classid here.", 
+                                         numericInput(inputId="ClassDrop", label="If you want to drop any new selected course, type in the classid 
+                                         here. Note that you cannot drop the courses that has previously enrolled for you in the system.", 
                                                       value=FALSE),
                                          actionButton("ClassDropConfirm", label="Submit"),
                                          conditionalPanel(condition="input.ClassDropConfirm", 
@@ -170,7 +169,8 @@ server <- function(input, output) {
       })
       # Record user's sid
       UserSid <- reactive({
-            dbGetQuery(dbcon, paste("SELECT * FROM Student WHERE sid =", input$StudentID))
+            temp <- dbGetQuery(dbcon, paste("SELECT * FROM Student WHERE sid =", input$StudentID))
+            temp %>% select(-tempid)
       })
       output$Student <- renderTable({
             data.frame(UserSid())
@@ -185,10 +185,11 @@ server <- function(input, output) {
             gender <- input$Gender
             email <- str_replace_all(string=name, pattern=" ", repl="")
             email <- paste0(email, "-c@my.cityu.edu.hk")
-            InsertStudent <- paste0("INSERT INTO Student (sid,sname,semail,sgender,DateOfBirth,phone,address) VALUES ('",
-                                    ID, "','", name, "','", email, "','", gender, "','','','')")
+            InsertStudent <- paste0("INSERT INTO Student (sid,sname,semail,sgender,DateOfBirth,phone,address,tempid) VALUES ('",
+                                    ID, "','", name, "','", email, "','", gender, "','','','','", ID, "')")
             dbSendQuery(dbcon, InsertStudent)
-            dbGetQuery(dbcon, paste("SELECT * FROM Student WHERE sid =", ID))
+            temp1 <- dbGetQuery(dbcon, paste("SELECT * FROM Student WHERE sid =", ID))
+            temp1 %>% select(-tempid)
       })
       
       output$NewStudent <- renderTable({
@@ -212,35 +213,54 @@ server <- function(input, output) {
             datatable(dbGetQuery(dbcon, ProfClassCourse), options=list(lengthMenu=c(15, 20), pageLength=15))
       })
       
+      # Creates an empty vector that ensures user does not enter the same class id twice
+      indicator <- as.numeric()
+      
       # Enroll TAB
       observeEvent(input$ClassIDConfirm, {
             
+            req(!(input$ClassID %in% indicator))
+
             # Operations in data frame
             TakenTable <- dbGetQuery(dbcon, "SELECT * FROM Taken")
-            # RegisTable <- data.frame(sid=input$StudentIDCheck)
+            RegTable <- dbGetQuery(dbcon, "SELECT * FROM Register")
+            classID <- as.integer(input$ClassID)
+            sID <- as.integer(input$StudentIDCheck)
+
+            indicator <- c(indicator, input$ClassID)  # First entry
             
             # If user input class id correctly
             if((input$ClassID %in% tempTable$classid)==T){
+                  
                   # Check whether the class is full
                   if((filter(tempTable, classid==input$ClassID)$available) == 0){
                         output$EnrollMessage <- renderText("Sorry, the course you chose is full.")
                   }else{
+                        
                         # Check whether the student has enrolled the course before
                         if((input$StudentIDCheck %in% TakenTable$sid)==T){ # If student in our record
+                              
                               # If we can find the record
                               if((as.numeric(filter(TakenTable, sid==input$StudentIDCheck)$cid) == filter(tempTable, classid==input$ClassID)$cid)==T){
                                     output$EnrollMessage <- renderText("Sorry, you have previously enrolled this course and here is your record:")
                                     output$EnrollOut <- renderTable(filter(TakenTable, sid==as.numeric(input$StudentIDCheck)))
                               }else{
-                                    # Else means that student can enroll the course and we need to update his/her schedule
-                                    output$EnrollMessage <- renderText("You have successfully enrolled the course.")
-                                    dbSendQuery(dbcon, paste0("INSERT INTO Register (sid,classid) VALUES ('", input$StudentIDCheck, "','", 
-                                                              input$ClassID, "')"))
+                                    # If student has previously enrolled this course, use "Register" table to check
+                                    if(classID %in% filter(RegTable, sid==sID)$classid){
+                                          output$EnrollMessage <- renderText("It looks like the system has enrolled this course for you already 
+                                                                             and please select another one.")
+                                    }else{
+                                          # Else means that student can enroll the course and we need to update his/her schedule
+                                          output$EnrollMessage <- renderText("You have successfully enrolled the course.")
+                                          dbSendQuery(dbcon, paste0("INSERT INTO Register (sid,classid,tempid) VALUES ('", input$StudentIDCheck, "','",
+                                                                    input$ClassID, "', '24')")) #24 is used for later deletion
+                                    }
                               }
-                        }else{ # New student 
+                              
+                        }else{ # New student
                               output$EnrollMessage <- renderText("You have successfully enrolled the course.")
-                              dbSendQuery(dbcon, paste0("INSERT INTO Register (sid,classid) VALUES ('", input$StudentIDCheck, "','", 
-                                                        input$ClassID, "')"))
+                              dbSendQuery(dbcon, paste0("INSERT INTO Register (sid,classid,tempid) VALUES ('", input$StudentIDCheck, "','",
+                                                        input$ClassID, "', '24')"))
                         }
                   }
             }
@@ -248,46 +268,52 @@ server <- function(input, output) {
             else{
                   output$EnrollMessage <- renderText("You have entered a invalid class ID!")
             }
+
       })
       
-   
+      
       # Schedule TAB
       output$StudentSchedule <- renderText({
             print("Here is your schedule next term.")
       })
       observeEvent(input$ClassIDConfirm, {
-            tempRegTable <- dbGetQuery(dbcon, "SELECT classid FROM Register")
+            tempRegTable <- dbGetQuery(dbcon, paste0("SELECT classid FROM Register WHERE sid = ", input$StudentIDCheck))
             tempMerge <- merge(tempTable, tempRegTable, by="classid")
             tempMerge <- tempMerge %>% select(-c(seats, available))
             output$StudentScheduleTable <- renderTable(tempMerge)
       })
+
       
       # Drop TAB
       observeEvent(input$ClassDropConfirm, {
-            tempRegTable <- dbGetQuery(dbcon, "SELECT classid FROM Register")
-            tempMerge <- merge(tempTable, tempRegTable, by="classid")
-            tempMerge <- tempMerge %>% select(-c(seats, available))
+            RegTable <- dbGetQuery(dbcon, "SELECT * FROM Register")
             
-            if((input$ClassDrop %in% tempMerge$classid)==T){
+            if(!is.null(input$ClassDrop) & (input$ClassDrop %in% RegTable$classid)==T){
+                  dbSendQuery(dbcon, paste0("DELETE FROM Register WHERE sid = ", input$StudentIDCheck, 
+                                            " AND classid = ", input$ClassDrop, " AND tempid = 24"))
                   output$DropMessage <- renderText("Here is your new schedule.")
-                  output$DropOut <- renderTable(filter(tempMerge, classid != input$ClassDrop))
+                  tempRegTable <- dbGetQuery(dbcon, paste0("SELECT classid FROM Register WHERE sid = ", input$StudentIDCheck))
+                  tempMerge <- merge(tempTable, tempRegTable, by="classid")
+                  tempMerge <- tempMerge %>% select(-c(seats, available))
+                  output$DropOut <- renderTable(tempMerge)
             }
-            
-      })
 
+      })
+      
+      
       ######################################################################
       # Quit
       observe({
             if(input$navbar == "stop"){
-                  # Remove any new insertion at the end
-                  dbSendQuery(dbcon, "DELETE FROM Student WHERE sid > 23")
-                  dbRemoveTable(dbcon, "Register")
+                  # Remove any new insertions at the end!!
+                  dbSendQuery(dbcon, "DELETE FROM Student WHERE tempid > 23")
+                  dbSendQuery(dbcon, "DELETE FROM Register WHERE tempid > 23")
+                  dbDisconnect(dbcon)
                   stopApp()
             }
       })
 }
 
-# dbRemoveTable(dbcon, "Register")
 # dbDisconnect(dbcon)
 
 # Run the application 
